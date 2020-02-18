a5c813b3ba9b7eac7e92b4e225c2f88b5a4ee2a7
Gkhancetiner <gok.cetiner@gmail.com>
2020-02-18 10:56:24 -0600

    no message

--- App.js
+++ App.js
@@ -2,9 +2,15 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const imageInfo = {
+    uri: "http://myawesomesite.com/myawesomeimage.png"
+
+  }
   return (
     <View style={styles.container}>
-      <Text style={styles.textColor}>Hey, i just edited this file!!!!</Text>
+
+      <Text style={styles.textColor}>Edited by Gokhan</Text>
+      <Image src={{imageInfo}}/>
     </View>
   );
 }
